ea06733956233aae48f9b3c631d4239c97c1c60f
Tomas Pytel <pytlicek@gmail.com>
2021-02-07 22:15:56 +0100

    Update setup.py bump version to 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sheet2dict",
-    version="0.0.3",
+    version="0.0.4",
     author="Tomas Pytel",
     author_email="pytlicek@gmail.com",
     description="Simple XLSX and CSV to dictionary converter",
