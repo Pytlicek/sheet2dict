438c430179a638dc596569939d7285e5f63ebfb5
Tomas Pytel <pytlicek@gmail.com>
2021-02-09 09:54:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sheet2dict",
-    version="0.0.5",
+    version="0.0.6",
     author="Tomas Pytel",
     author_email="pytlicek@gmail.com",
     description="Simple XLSX and CSV to dictionary converter",
