e07c1a7c1b329be38b72651dd5f98dc79677d918
byb <baiyibaicyou@gmail.com>
2021-07-11 22:44:26 +0800

    Allows a user to select a different sheet from a file with multiple sheets. Now with tests.

--- tests/test_1_xlsx.py
+++ tests/test_1_xlsx.py
@@ -6,32 +6,31 @@ def test_empty_object(worksheet):
 
 def test_parse_xlsx_header(worksheet):
     ws = worksheet
-    ws.xlsx_to_dict(path="tests/inventory.xlsx")
+    ws.xlsx_to_dict(path="tests/inventory.xlsx", select_sheet='SJ3')
     ws_header = ws.header
     assert ws_header == {
-        "country": "SK",
-        "city": "Bratislava",
-        "citizens": "400000",
-        None: "11",
-        "random_field": "cc",
+        "country": "Taiwan",
+        "city": "Taipei",
+        "area (km)": "271.8",
+        None: "88.0",
+        "rand_field": "ee",
     }
     assert None in ws_header
 
 
 def test_parse_xlsx_all_items(worksheet):
     ws = worksheet
-    ws_items = ws.xlsx_to_dict(path="tests/inventory.xlsx")
-    assert "Bratislava" in str(ws_items)
-    assert "Miami" in str(ws_items)
-    assert "None" in str(ws_items)
+    ws_items = ws.xlsx_to_dict(path="tests/inventory.xlsx", select_sheet='SJ3')
+    assert "Taipei" in str(ws_items)
+    assert "Seoul" in str(ws_items)
 
 
 def test_parse_xlsx_sheet_items(worksheet):
     ws = worksheet
-    ws.xlsx_to_dict(path="tests/inventory.xlsx")
+    ws.xlsx_to_dict(path="tests/inventory.xlsx", select_sheet='SJ3')
     ws_items = ws.sheet_items
-    assert "Bratislava" in str(ws_items)
-    assert "Miami" in str(ws_items)
+    assert "Taipei" in str(ws_items)
+    assert "Bratislava" not in str(ws_items)
     assert "None:" in str(ws_items)
     assert None in ws_items[0]
     assert len(ws_items) > 1
@@ -40,10 +39,10 @@ def test_parse_xlsx_sheet_items(worksheet):
 
 def test_sanitize_sheet_items(worksheet):
     ws = worksheet
-    ws.xlsx_to_dict(path="tests/inventory.xlsx")
+    ws.xlsx_to_dict(path="tests/inventory.xlsx", select_sheet='SJ3')
     ws_items = ws.sanitize_sheet_items
-    assert "Bratislava" in str(ws_items)
-    assert "Miami" in str(ws_items)
+    assert "Taipei" in str(ws_items)
+    assert "Bratislava" not in str(ws_items)
     assert "None:" not in str(ws_items)
     assert None not in ws_items[0]
 
@@ -57,16 +56,15 @@ xlsx_file = BytesIO(xlsx_file.read())
 
 def test_parse_xlsx_all_items_as_object(worksheet):
     ws = worksheet
-    ws_items = ws.xlsx_to_dict(path=xlsx_file)
-    assert "Bratislava" in str(ws_items)
-    assert "Miami" in str(ws_items)
-
+    ws_items = ws.xlsx_to_dict(path=xlsx_file, select_sheet='SJ3')
+    assert "Taipei" in str(ws_items)
+    assert "Bratislava" not in str(ws_items)
 
 def test_parse_xlsx_sheet_items_as_object(worksheet):
     ws = worksheet
-    ws.xlsx_to_dict(path=xlsx_file)
+    ws.xlsx_to_dict(path=xlsx_file, select_sheet='SJ3')
     ws_items = ws.sheet_items
-    assert "Bratislava" in str(ws_items)
-    assert "Miami" in str(ws_items)
+    assert "Taipei" in str(ws_items)
+    assert "Bratislava" not in str(ws_items)
     assert len(ws_items) > 1
     assert len(ws_items) == 6
