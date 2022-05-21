677addb25c801c0c4ae31026389bdb0e36f385de
Tomas Pytel <pytlicek@gmail.com>
2022-05-21 13:57:59 +0200

    fix build system

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf dist/*
-python3 setup.py sdist bdist_wheel && python3 -m twine upload --repository testpypi dist/*
-#python3 setup.py sdist bdist_wheel && python3 -m twine upload dist/*
+python3 -m build
+python3 -m twine upload --repository testpypi dist/*
